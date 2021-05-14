663c40ffeafc56dfa5e40e6e5e6c43b243c7635f
eugenekovalenko <eugenekovalenko0@gmail.com>
2021-05-14 12:01:03 +0300

    added routing on client

--- client/src/components/app/app.tsx
+++ client/src/components/app/app.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
+import { Home } from '../Home';
+import { Host } from '../Host';
+import { Listing } from '../Listing';
 import { Listings } from '../Listings';
+import { NotFound } from '../NotFound';
+import { User } from '../User';
 
 export function App() {
   return (
     <div className="App">
-        <Listings title="House Listings"/>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/host" component={Host}/>
+          <Route exact path="/listing/:id" component={Listing}/>
+          <Route exact path="/listings/:location?" component={Listings}/>
+          <Route exact path="/user/:id" component={User}/>
+          <Route component={NotFound}/>
+        </Switch>
     </div>
   );
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import ApolloClient from 'apollo-boost'
 import {ApolloProvider} from 'react-apollo'
 import {App} from './components/app';
@@ -11,7 +12,9 @@ const client = new ApolloClient({
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ApolloProvider>,
   document.getElementById('root')
 );
