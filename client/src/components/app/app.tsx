14b8146fe2a0424e71a97239f52abd61c6d55ebe
eugenekovalenko <eugenekovalenko0@gmail.com>
2021-05-15 11:20:19 +0300

    buided the ui fror login page

--- client/src/components/app/app.tsx
+++ client/src/components/app/app.tsx
@@ -4,22 +4,25 @@ import { Home } from '../Home';
 import { Host } from '../Host';
 import { Listing } from '../Listing';
 import { Listings } from '../Listings';
+import { Login } from '../Login';
 import { NotFound } from '../NotFound';
 import { User } from '../User';
-
+import { Layout } from "antd";
+import '../../styles/index.css'
 
 export function App() {
   return (
-    <div className="App">
+    <Layout id="app">
         <Switch>
           <Route exact path="/" component={Home}/>
           <Route exact path="/host" component={Host}/>
           <Route exact path="/listing/:id" component={Listing}/>
           <Route exact path="/listings/:location?" component={Listings}/>
+          <Route exact path="/login" component={Login}/>
           <Route exact path="/user/:id" component={User}/>
           <Route component={NotFound}/>
         </Switch>
-    </div>
+    </Layout>
   );
 }
 
