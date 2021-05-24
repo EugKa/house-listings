9113f65ccd40dda30bf12993a768aa5c24f0adc5
eugenekovalenko <eugenekovalenko0@gmail.com>
2021-05-24 14:26:38 +0300

    created the ListingCreateBooking component

--- client/src/components/Listing/Listing.tsx
+++ client/src/components/Listing/Listing.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
+import { Moment } from 'moment'
 import { useQuery } from '@apollo/react-hooks'
 import { LISTING } from '../../lib/graphql/queries'
 import { Listing as ListingData ,ListingVariables } from '../../lib/graphql/queries/Listing/__generated__/Listing'
 import { RouteComponentProps } from 'react-router'
 import { Layout, Row, Col } from 'antd'
 import { PageSkeleton, ErrorBanner } from '../../lib/components'
-import { ListingBookings, ListingDetails } from '../Listing/components'
+import { ListingBookings, ListingDetails, ListingCreateBooking } from '../Listing/components'
 
 interface MatchParams {
     id: string;
@@ -15,7 +16,9 @@ const PAGE_LIMIT = 3;
 const { Content } = Layout;
 
 export const Listing = ({match}: RouteComponentProps<MatchParams>) => {
-    const [bookingsPage, setBookingsPage] = useState(1)
+    const [bookingsPage, setBookingsPage] = useState(1);
+    const [checkInDate, setCheckInDate] = useState<Moment | null>(null)
+    const [checkOutDate, setCheckOutDate] = useState<Moment | null>(null)
     const { loading, data, error } = useQuery<ListingData ,ListingVariables>(LISTING, {
         variables: {
             id: match.params.id,
@@ -53,7 +56,17 @@ export const Listing = ({match}: RouteComponentProps<MatchParams>) => {
             limit={PAGE_LIMIT} 
             setBookingsPage={setBookingsPage}
         />
-        ): null;
+    ): null;
+
+    const listingCreateBooking = listing ? (
+        <ListingCreateBooking 
+            price={listing.price}
+            checkInDate={checkInDate}
+            checkOutDate={checkOutDate}
+            setCheckInDate={setCheckInDate}
+            setCheckOutDate={setCheckOutDate}
+        />
+    ) : null;
 
     return (
         <Content className="listings">
@@ -61,6 +74,9 @@ export const Listing = ({match}: RouteComponentProps<MatchParams>) => {
                 <Col xs={24} lg={14}>
                     {ListingDetailsElement}
                     {ListingBookingsElement}
+                </Col>
+                <Col xs={24} lg={10}>
+                    {listingCreateBooking}
                 </Col>
             </Row>
         </Content>
