499235c4c97f3293ccf9beb5a57e1efcad95b50a
eugenekovalenko <eugenekovalenko0@gmail.com>
2021-05-24 11:24:10 +0300

    querying for listing data

--- client/src/components/Listings/Listings.tsx
+++ client/src/components/Listings/Listings.tsx
@@ -1,6 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { LISTING } from '../../lib/graphql/queries'
+import { Listing as ListingData ,ListingVariables } from '../../lib/graphql/queries/Listing/__generated__/Listing'
+import { RouteComponentProps } from 'react-router'
+import { Layout } from 'antd'
+import { PageSkeleton, ErrorBanner } from '../../lib/components'
 
-export const Listings = () => {
+interface MatchParams {
+    id: string;
+}
+
+const PAGE_LIMIT = 3;
+const { Content } = Layout;
+
+export const Listings = ({match}: RouteComponentProps<MatchParams>) => {
+    const [bookingsPage, setBookingsPage] = useState(1)
+    const { loading, data, error } = useQuery<ListingData ,ListingVariables>(LISTING, {
+        variables: {
+            id: match.params.id,
+            bookingsPage,
+            limit: PAGE_LIMIT
+        }
+    })
+
+    if(loading) {
+        return (
+            <Content className="listings">
+                <PageSkeleton/>
+            </Content>
+        )
+    }
+
+    if(error) {
+        return (
+            <Content className="listings">
+                <ErrorBanner description="This listing may not exist or we've ecncountered an error. Please try againg soon!"/>
+                <PageSkeleton/>
+            </Content>
+        )
+    }
+
+    const listing = data ? data.listing : null;
+    const listingBookings = listing ? listing.bookings : null;
+
     return (
         <div>
             Listings
